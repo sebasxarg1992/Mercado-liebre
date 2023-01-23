67389150e8a14000197bf2aaa7ebf55dd95003df
unknown <sebamolina1992@gmail.com>
2023-01-23 17:52:04 -0300

    agregando archivos

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+// levantar nuestro servidor
+const port = process.env.PORT || 3000;
+app.listen(port,()=>console.log(`Servidor corriendo en el puerto ${port}`))
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
